7be6f50c86c1b39e221987d52fa09290c94b4e63
Giovanni Win10 Giorgi <jj@gioorgi.com>
2017-02-05 21:51:23 +0100

    Used poweful NewPing v1.8 to track down distance

--- examples/auto-brake/auto-brake.ino
+++ examples/auto-brake/auto-brake.ino
@@ -1,13 +1,22 @@
 // -*- mode:c++; mode: flymake -*-
 /**
-   This Small examples uses a
-   HC RS04 ultrasonic sensor on pin 7,8 to
+   This Small examples uses a  HC RS04 ultrasonic sensor on pin 7,8 to
    show an auto brake function.
    Mount the arduino with the sensor on the car.
    Define the Security distance (based on your project)
 
-   The system will issue a power function brake on  first channel
-   if the sensor detect a object in a range <= AutoBrakeDistanceCm
+   
+   If the sensor detect a object in a range <= AutoBrakeDistanceCm
+   Arduino system will issue a power function brake on first channel, possibly overriding
+   your orders.
+
+   The NewPing v 1.8 is used for better measurements: for more information see https://bitbucket.org/teckel12/arduino-new-ping/wiki/Home
+  
+TODO: TRY TO COMPUTE RELATIVE SPEED VIA FORMULA LIKE
+Speed = (FirstDistance - SecondDistance) / (SecondTime - FirstTime)
+
+
+   Note: there are 1,000 microseconds in a millisecond and 1,000,000 microseconds in a second.
 */
 // CONFIG
 const int triggerPort = 7;
@@ -15,23 +24,20 @@ const int echoPort = 8;
 const int PowerFunctionPort = 12;
 const int AutoBrakeDistanceCm = 10;
 
-//const int blueLed = 11;
-//const int blueLed2=11;
-//const int redLed = 2;   // SIGNAL A slowness in the main loop (=Arduino take too time to response, i.e. brake could be ineffective)
-
-// Depends from sensor. See its datasheet.
-const int maxDistCm = 28;
-const int minDistCm = 10;
-
+//  HCRS04 Specs talk about 400cm. NewPing default is 500
+const int MaxDistCm = 400;
 
 
 #include <PowerFunctions.h>
+#include "NewPing.h"
 
-
+// Decomment your preferred debug
 #define log(x)  Serial.print(F(x))
+//#define log(x)
 
 
 PowerFunctions powerFunction(12, 0);
+NewPing sonar(triggerPort, echoPort, MaxDistCm);
 
 void setup() {
 
@@ -40,103 +46,59 @@ void setup() {
   //pinMode( blueLed, OUTPUT);
   //pinMode(redLed, OUTPUT);
   Serial.begin( 9600 );
-  Serial.println( F( "AutoBrake v1.1"));
+  Serial.println( F( "AutoBrake v1.2+"));
+  // 
+  // Make a two break cmd (test set)
+  //
+  powerFunction.combo_pwm(PWM_BRK, PWM_BRK);
+  powerFunction.combo_pwm(PWM_BRK, PWM_BRK);
 
 }
 
 void loop() {
 
-  // Ultrasonic Scan
-  static long lastDistance = 0;
+  // Static registered last distance
+  static unsigned long lastDistance = 0;
   //analogWrite(blueLed, 255);
 
-  // 
-  // Make a two break cmd (test set)
-  powerFunction.combo_pwm(PWM_BRK, PWM_BRK);
-  powerFunction.combo_pwm(PWM_BRK, PWM_BRK);
 
-  while (true) {
-    unsigned long startTime = millis();
-    //porta bassa l'uscita del trigger
-    digitalWrite( triggerPort, LOW );
+  unsigned long startTime = millis();
 
-    //invia un impulso di 10microsec su trigger
-    digitalWrite( triggerPort, HIGH );
-    delayMicroseconds( 10 );
-    digitalWrite( triggerPort, LOW );
+  // We do 3 short measures to gain better accuracy
+  //unsigned long r = sonar.ping_cm();
+  unsigned long r = sonar.convert_cm(sonar.ping_median(3));
 
-    // Reads a pulse (either HIGH or LOW) on a pin.
-    // Returns the length of the pulse (in microseconds) or 0 if no pulse is completed before the timeout
-    // Note: there are 1,000 microseconds in a millisecond and 1,000,000 microseconds in a second.
-    const  unsigned long MicrosecondsPulseTimeout =  38000; // 38ms
-    unsigned long duration = pulseIn( echoPort, HIGH, MicrosecondsPulseTimeout);
-    if (duration == 0 ) {
-      log("PULSE TIMEOUT");
 
-    } else  {
-      // cm
-      // BAD float r = 0.034 * duration / 2;
-      float r=  duration /58;
-      if (r != lastDistance) {
+  
+  if (r == 0 ) {
+    // Could also mean there are no obstacles and live happily...
+    //log("OUT OF RANGE");
+  } else  {
+    if (r != lastDistance) {
+      log("> ") ;
+      Serial.println( r );
+      log("cm") ;
+      //int ledValue=map(rlong,minDistCm*10,maxDistCm*10,0,255);
 
-        log("dur: ");
-        Serial.print( duration );
-        log("> ") ;
-
-        //After 38ms it is out of sensor range:
-        if ( duration > 38000 ) {
-          log("out of range");
-        } else {
-          Serial.print( r ); log("cm") ;
-          long rlong = (long)r * 10;
-          log(" >Res");
-          Serial.print(rlong);
-
-          //int ledValue=map(rlong,minDistCm*10,maxDistCm*10,0,255);
-          //Serial.print(F(" "));
-          //Serial.print( ledVaue );
-          if (r >= minDistCm && r <= AutoBrakeDistanceCm ) {
-            log("!");
-            // Brake! (short way)
-            powerFunction.combo_pwm(PWM_BRK, PWM_BRK);
-            //analogWrite(blueLed, ledValue);
-          } else {
-            // TODO: Push the old one (lastDistance)
-            // Serial.print(F(".."));
-          }
-          /// PUT HERE Other low priorty housekeeping
-        }
-        lastDistance = r;
+      //TODO: Implement a sort of progressive brake to avoid too much trouble
+      // The system should be able to extimate current velocity
+      if (  r <= AutoBrakeDistanceCm ) {
+        log("!BRAKE|\n");
+        // Brake! (short way)
+        powerFunction.combo_pwm(PWM_BRK, PWM_BRK);
+        //analogWrite(blueLed, ledValue);
+      } else {
+        // TODO: Push the old one (lastDistance)
+        // Serial.print(F(".."));
       }
+      /// PUT HERE Other low priorty housekeeping
+        
+      lastDistance = r;
       log("\n");
     }
-    /* POST DELAY
-       From sensor data sheet:
-       -----
-       we suggest to use over 60ms
-       measurement cycle, in order to prevent trigger signal
-       to the echo signal
-
-       60*5 --> too slow
-       60*2 --> good until 28 cm
-       ----
-       NB: Interrupt could lead to fake measure, so we delay 60ms
-
-    */
-
-    unsigned long endTime = millis() - startTime;
-    const int delayTime = 60 * 2   /* + DEBUG 1000 */;
-    if (endTime < delayTime ) {
-      // DELTA DELAY: add a 1 millis for overhead
-      delay( (delayTime-endTime) + 1 );
-      //digitalWrite(redLed, LOW);
-    } else {
-      // SIGNAL a bad condition and pause a bit
-      //digitalWrite(redLed, HIGH);
-      delay(1000);
-    }
-    
-  } // main while loop
+     
+  }
+  
 }
 
 
